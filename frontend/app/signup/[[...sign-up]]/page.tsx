9cd41806f352bd00b0757610ae8669a5839c32b2
Rishabh Dotasara <dotasararishabh@gmail.com>
2025-12-01 15:18:00 +0530

    some more bug fixes

--- frontend/app/signup/[[...sign-up]]/page.tsx
+++ frontend/app/signup/[[...sign-up]]/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Signup() {
   return (
     <div className='h-screen w-full bg-gray-100 flex items-center justify-center'>
-      <SignUp forceRedirectUrl={"/onboard-user"}/>
+      <SignUp forceRedirectUrl={"/"}/>
     </div>
   )
 }
